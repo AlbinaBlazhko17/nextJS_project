1df0cecfcd89e9aa6ce2ca4c95d3e456c129149e
Albinator <albinamivina@gmail.com>
2023-03-14 11:29:15 +0200

    project init and configure eslint, stylelint

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "amd": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react/jsx-runtime"
+    ],
+    "overrides": [
+        {
+            // enable the rule specifically for TypeScript files
+            "files": ["*.ts", "*.mts", "*.cts", "*.tsx"],
+            "rules": {
+              "semi": "off",
+              "@typescript-eslint/semi": "warn",
+              "@typescript-eslint/explicit-function-return-type": "error"
+            }
+          }
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    },
+    
+};
