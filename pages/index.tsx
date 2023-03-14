6d31c4aa302dbf247cf8b5efbb812e88c82a9c1a
Albinator <albinamivina@gmail.com>
2023-03-14 12:51:06 +0200

    add component for h tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,17 @@ import React, { Component }  from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { Htag } from '@/components';
+
 import styles from '@/styles/Home.module.css';
+
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home(): JSX.Element {
   return (
-    <>
-      dfsdfsdfs
-    </>
+    <div>
+      <Htag tag='h1'> Children </Htag>
+    </div>
   );
 }
