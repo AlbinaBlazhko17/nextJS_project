43d5d28dd3a04bc698bd3ac5946f397a362cbb55
Albinator <albinamivina@gmail.com>
2023-03-14 12:17:10 +0200

    add fonts and variables of basic colors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Mytop - the best top</title>
+      <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"/>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'anonymous'}/>
+      <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+Display:wght@300;400;500;700&family=Sen:wght@400;700&display=swap" rel="stylesheet"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>;
 }
