63d083c5f69da40dd09929786d261cb70120986d
Albinator <albinamivina@gmail.com>
2023-03-16 14:34:15 +0200

    add grid to layout

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 import cn from 'classnames';
+import { format } from 'date-fns';
 import { FooterProps } from './Footer.props';
 
 import styles from './Footer.module.css';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
 	return (
-		<div {...props}>
-			Footer
-		</div>
+		<footer className={cn(className, styles.wrapper)} {...props} >
+			<div>
+				OwlTop © 2022 - {format(new Date(), 'yyyy')} All right reserved
+			</div>
+			<a href='#' target='_blank' rel='noopener noreferrer'>User Agreement</a>
+			<a href='#' target='_blank' rel='noopener noreferrer'>Private Policy</a>
+		</footer>
 	);
 };
