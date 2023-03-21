e83647f534fc7d8e8f3ba1f415010e2bc597d492
Albinator <albinamivina@gmail.com>
2023-03-21 11:58:05 +0200

    get categories from API

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
 import React, { Component, useState }  from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
-import { Inter } from 'next/font/google';
+import { Inter, Unkempt } from 'next/font/google';
+import axios, { AxiosResponse } from 'axios';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+
 import { Button, Htag, P, Rating } from '@/components';
 import { withLayout } from '@/layout';
+import { MenuItem } from '@/interfaces/menu.interface';
 
 import styles from '@/styles/Home.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
-function Home(): JSX.Element {
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory?: number;
+}
+
+function Home({ menu }: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
   const [rating, setRating] = useState<number>(4);
 
   return (
@@ -21,8 +30,25 @@ function Home(): JSX.Element {
       <P>Medium paragraph</P>
       <P textSize='L'>Large paragraph</P>
       <Rating rating={rating} isEditable setRating={setRating}/>
+      <ul>
+        { menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>)) }
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props:  {
+      menu,
+      firstCategory
+    }
+  };
+};
+
