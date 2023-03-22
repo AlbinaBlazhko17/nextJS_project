8d68d7ec5072e1f54b694d7b5a7beaf1c1f7711e
Albinator <albinamivina@gmail.com>
2023-03-22 11:52:25 +0200

    add logo with styles

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -2,11 +2,16 @@ import cn from 'classnames';
 import { SidebarProps } from './Sidebar.props';
 import { Menu } from '../Menu/Menu';
 
+import Logo from '../logo.svg';
+
 import styles from './Sidebar.module.css';
 
-export const Sidebar = ({ ...props }: SidebarProps): JSX.Element => {
+
+export const Sidebar = ({ className, ...props }: SidebarProps): JSX.Element => {
 	return (
-		<div {...props}>
+		<div {...props} className={cn(className, styles.sidebar)}>
+			<Logo className={styles.logo}/>
+			<div>Search</div>
 			<Menu />
 		</div>
 	);
