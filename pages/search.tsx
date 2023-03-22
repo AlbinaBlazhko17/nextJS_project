f2cbd0efc2aa552f60a79f17ff9602660dae9616
Albinator <albinamivina@gmail.com>
2023-03-22 11:40:46 +0200

    add index.tsx for first categories

--- pages/search.tsx
+++ pages/search.tsx
@@ -12,7 +12,9 @@ function Search(): JSX.Element {
 		</>
 	);
 }
+
 export default withLayout(Search);
+
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 	const firstCategory = 0;
 	const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
