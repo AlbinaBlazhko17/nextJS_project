7ac26397ee4e7668554d4e7400a412886a89f216
Albinator <albinamivina@gmail.com>
2023-03-24 10:54:32 +0200

    add routing to search

--- components/Search/Search.tsx
+++ components/Search/Search.tsx
@@ -1,5 +1,7 @@
 import cn from 'classnames';
-import { useState } from 'react';
+import { useState, KeyboardEvent } from 'react';
+import { useRouter } from 'next/router';
+
 import { SearchProps } from "./Search.props";
 import { Input } from '../Input/Input';
 import { Button } from '../Button/Button';
@@ -9,9 +11,26 @@ import SearchIcon from './Search.svg';
 import styles from './Search.module.css';
 
 
-
 export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
 	const [search, setSearch] = useState<string>('');
+	const router = useRouter();
+
+	const goToSearch = (): void => {
+		router.push({
+			pathname: '/search',
+			query: {
+				q: search
+			}
+		});
+	};
+
+	const handleKeyDown = (e: KeyboardEvent): void => {
+		console.log('Work');
+		if(e.key === 'Enter' || e.key === 'return') {
+			console.log('Enter');
+			goToSearch();
+		}
+	};
 
 	return (
 		<div className={cn(className, styles.search)} {...props}>
@@ -20,10 +39,12 @@ export const Search = ({ className, ...props }: SearchProps): JSX.Element => {
 				value={search}
 				onChange={(e): void => setSearch(e.target.value)}
 				className={styles.input}
+				onKeyDown={handleKeyDown}
 			/>
 			<Button 
 				appearance='primary'
 				className={styles.button}
+				onClick={goToSearch}
 			>
 				<SearchIcon/>
 			</Button>
