a8722a88a588c4acae27bf8e4bca57b5d036aebc
Albinator <albinamivina@gmail.com>
2023-03-24 11:26:45 +0200

    add data to product

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,11 +1,26 @@
 import { ProductProps } from './Product.props';
+import { Card } from '../Card/Card';
+import { Rating } from '../Rating/Rating';
+import { Tag } from '../Tag/Tag';
 
 import styles from './Product.module.css';
 
+
+
 export const Product = ({ product, ...props }: ProductProps): JSX.Element => {
 	return (
-		<div>
-			{product.title}
-		</div>
+		<Card>
+			<div className={styles.logo}>
+				<img src={product.image} alt={product.title} />
+			</div>
+			<div className={styles.title}>{product.title}</div>
+			<div className={styles.price}>{product.price}</div>
+			<div className={styles.credit}>{product.credit}</div>
+			<div className={styles.rating}><Rating rating={product.reviewAvg ?? product.initialRating} /></div>
+			<div className={styles.tag}>{product.categories.map(c => (<Tag key={c} color='ghost'>{c}</Tag>))}</div>
+			<div className={styles.priceTitle}>цена</div>
+			<div className={styles.creditTitle}>в кредит</div>
+			<div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
+		</Card>
 	);
 };
