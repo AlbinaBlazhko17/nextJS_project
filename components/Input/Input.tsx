4d67e2b4dd3af95196cba626c9b4616c1c9fa5d2
Albinator <albinamivina@gmail.com>
2023-03-28 10:28:48 +0300

    add ref to Input component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,10 +1,12 @@
 import cn from 'classnames';
+import React, { ForwardedRef, forwardRef } from 'react';
 import { InputProps } from './Input.props';
 
 import styles from './Input.module.css';
 
-export const Input = ({ className, ...props }: InputProps): JSX.Element => {
+
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input ({ className, ...props }: InputProps, ref): JSX.Element {
 	return (
-		<input className={cn(className, styles.input)} {...props} />
+		<input className={cn(className, styles.input)} ref={ref} {...props} />
 	);
-};
+});
