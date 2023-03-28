b040f5720c27fe5a83601425d5a7074dd0a62474
Albinator <albinamivina@gmail.com>
2023-03-28 10:31:02 +0300

    add ref to Textarea component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,11 +1,11 @@
 import cn from 'classnames';
-import React, { ForwardedRef, forwardRef } from 'react';
+import { forwardRef } from 'react';
 import { InputProps } from './Input.props';
 
 import styles from './Input.module.css';
 
 
-export const Input = React.forwardRef<HTMLInputElement, InputProps>(function Input ({ className, ...props }: InputProps, ref): JSX.Element {
+export const Input = forwardRef<HTMLInputElement, InputProps>(function Input ({ className, ...props }: InputProps, ref): JSX.Element {
 	return (
 		<input className={cn(className, styles.input)} ref={ref} {...props} />
 	);
--- components/Textarea/Textarea.tsx
+++ components/Textarea/Textarea.tsx
@@ -2,9 +2,10 @@ import cn from 'classnames';
 import { TextareaProps } from './Textarea.props';
 
 import styles from './Textarea.module.css';
+import { forwardRef } from 'react';
 
-export const Textarea = ({ className, ...props }: TextareaProps): JSX.Element => {
+export const Textarea = forwardRef<HTMLAreaElement, TextareaProps>(function Textarea({ className, ...props }: TextareaProps): JSX.Element {
 	return (
 		<textarea className={cn(className, styles.textarea)} {...props} />
 	);
-};
+});
