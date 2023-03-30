2555d4e86ca0c38ada6e469c8b62d516b0bddd7a
Albinator <albinamivina@gmail.com>
2023-03-30 11:50:04 +0300

    add accesibility with tab key in body

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from 'react';
+import { FunctionComponent, KeyboardEvent, useState, useRef } from 'react';
 import cn from 'classnames';
 import { LayoutProps } from './Layout.props';
 import { Header, Sidebar, Footer } from './index';
@@ -9,11 +9,29 @@ import { Up } from '@/components';
 
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+	const [isSkipLinkDisplayed, setIsSkipLinkDisplayed] = useState<boolean>(false);
+	const bodyRef = useRef<HTMLDivElement>(null);
+	const skipContentAction = (key: KeyboardEvent): void => {
+		if (key.code == 'Space' || key.code == 'Enter') {
+			key.preventDefault();
+			bodyRef.current?.focus();
+		}
+		setIsSkipLinkDisplayed(false);
+	};
+
 	return(
 		<div className={styles.wrapper}>
+			<a
+				onFocus={(): void => setIsSkipLinkDisplayed(true)}
+				tabIndex={1}
+				className={cn(styles.skipLink, {
+					[styles.displayed]: isSkipLinkDisplayed
+				})}
+				onKeyDown={skipContentAction}
+			>Сразу к содержанию</a>
 		<Header className={styles.header}/>
 		<Sidebar className={styles.sidebar}/>
-		<div className={styles.body}>
+		<div className={styles.body} ref={bodyRef} tabIndex={0}>
 			{children}
 		</div>
 		<Footer className={styles.footer}/>
